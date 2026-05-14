7beb3cdf7ccaaf65a853d12d19547b3a7419e3a4
MrGuyman99 <bodiepagelow@gmail.com>
2026-05-13 21:00:59 -0500

    Switched to unordered_map

--- src/morse.cpp
+++ src/morse.cpp
@@ -1,28 +1,31 @@
 #include "morse.hpp"
 #include <fstream>
 #include <iostream>
-
-// Macro So We don't have to write an insane amount of if statements
-#define CheckCharacter(check_character, compare_character) \
-	({                                                     \
-		do {                                               \
-			if (check_character == compare_character) {    \
-				std::cout << ".-\n";                       \
-			}                                              \
-		} while (0);                                       \
-	})
+#include <unordered_map>
 
 void morse::cpp_to_morse(std::string FilePath) {
-	std::fstream convert_file(FilePath);
-	std::string current_character;
-	if (!convert_file.is_open()) {
+	std::fstream og_file(FilePath);
+	char current_character;
+	std::string converted_file;
+	std::unordered_map<char, std::string> morse_translate = {
+		{'A', ".-"},   {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+		{'E', "."},	   {'F', "..-."}, {'G', "--."},	 {'H', "...."},
+		{'I', ".."},   {'J', ".---"}, {'K', "-.-"},	 {'L', ".-.."},
+		{'M', "--"},   {'N', "-."},	  {'O', "---"},	 {'P', ".--."},
+		{'Q', "--.-"}, {'R', ".-."},  {'S', "..."},	 {'T', "-"},
+		{'U', "..-"},  {'V', "...-"}, {'W', ".--"},	 {'X', "-..-"},
+		{'Y', "-.--"}, {'Z', "--.."}, {' ', "/"}};
+
+	if (!og_file.is_open()) {
 		std::cerr << "Could not open file at path " << FilePath << std::endl;
 		return;
 	}
-	// getline reads character by character
-	while (getline(convert_file, current_character)) {
-		CheckCharacter(current_character, "A");
+	// Reading the file character by character and translating it
+	while (og_file.get(current_character)) {
+		converted_file += morse_translate[current_character];
+		converted_file += " ";
 	}
-	convert_file.close();
+	std::cout << converted_file << std::endl;
+	og_file.close();
 }
 void morse::morse_to_cpp(std::string FilePath) {}
