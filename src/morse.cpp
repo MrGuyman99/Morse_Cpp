8231c7f6068935a255e89a8d499c6ff133d5dc3b
MrGuyman99 <bodiepagelow@gmail.com>
2026-05-14 20:31:15 -0500

    Added all supported characters

--- src/morse.cpp
+++ src/morse.cpp
@@ -3,21 +3,26 @@
 #include <iostream>
 #include <unordered_map>
 
+// Translates characters into morse, puts spaces between each morse character
+// Ignores things if they aren't supported
 void morse::cpp_to_morse(std::string FilePath) {
 	std::ifstream og_file(FilePath);
 	char current_character;
 	std::string converted_file;
-	// Map of all the characters and their morse code counterparts
 	std::unordered_map<char, std::string> morse_translate = {
-		{'a', ".-"},   {'b', "-..."}, {'c', "-.-."}, {'d', "-.."},
-		{'e', "."},	   {'f', "..-."}, {'g', "--."},	 {'h', "...."},
-		{'i', ".."},   {'j', ".---"}, {'k', "-.-"},	 {'l', ".-.."},
-		{'m', "--"},   {'n', "-."},	  {'o', "---"},	 {'p', ".--."},
-		{'q', "--.-"}, {'r', ".-."},  {'s', "..."},	 {'t', "-"},
-		{'u', "..-"},  {'v', "...-"}, {'w', ".--"},	 {'x', "-..-"},
-		{'y', "-.--"}, {'z', "--.."}, {' ', "/"}};
+		{'a', ".-"},	 {'b', "-..."},	  {'c', "-.-."},   {'d', "-.."},
+		{'e', "."},		 {'f', "..-."},	  {'g', "--."},	   {'h', "...."},
+		{'i', ".."},	 {'j', ".---"},	  {'k', "-.-"},	   {'l', ".-.."},
+		{'m', "--"},	 {'n', "-."},	  {'o', "---"},	   {'p', ".--."},
+		{'q', "--.-"},	 {'r', ".-."},	  {'s', "..."},	   {'t', "-"},
+		{'u', "..-"},	 {'v', "...-"},	  {'w', ".--"},	   {'x', "-..-"},
+		{'y', "-.--"},	 {'z', "--.."},	  {' ', "/"},	   {'1', ".----"},
+		{'2', "..---"},	 {'3', "...--"},  {'4', "....-"},  {'5', "....."},
+		{'6', "-...."},	 {'7', "--..."},  {'8', "--.."},   {'9', "----."},
+		{'0', "-----"},	 {'?', "..--.."}, {'!', "-.-.--"}, {'.', ".-.-.-"},
+		{',', "--..--"}, {';', "-.-.-."}, {':', "---..."}, {'+', ".-.-."},
+		{'-', "-....-"}, {'/', "-..-."},  {'=', "-..-"}};
 
-	// If the file isn't open at all exit the program
 	if (!og_file.is_open()) {
 		std::cerr << "Could not open file at path " << FilePath << std::endl;
 		return;
@@ -25,26 +30,17 @@ void morse::cpp_to_morse(std::string FilePath) {
 	// Reading the file character by character and translating it
 	while (og_file.get(current_character)) {
 		try {
-			// If a supported character is selected translate it and add it to
-			// the final string
 			converted_file += morse_translate.at(current_character);
 			converted_file += " ";
 		} catch (const std::out_of_range& e) {
-			// If an unsupported character is selected first ignore it
 			std::cerr << "Character Not Supported! Character is: '"
 					  << current_character << "' Ignoring\n";
-			// If the last character was a space delete it
-			// This is so things like () and "" work properly Take ( " Hello " )
-			// compared to ("Hello")
 			if (!converted_file.empty() && converted_file.back() == ' ') {
 				converted_file.pop_back();
 			}
 			converted_file += current_character;
 		}
 	}
-	// Close the read only file, open a write file, then append the entire
-	// converted string to it
-	// TODO: Completely convert the file, appending is just for testing
 	og_file.close();
 	std::ofstream write_file(FilePath, std::ios::app);
 	write_file << std::endl << "--------------\n" << converted_file;
